7be50280413d818cfda24503ff23a2f01d71cc20
Wiradp <wiradp@yahoo.com>
2025-06-25 08:22:57 +0700

    Modify startup.sh

--- startup.sh
+++ startup.sh
@@ -1,1 +1,9 @@
-gunicorn -b 0.0.0.0:8000 --timeout 600 app:app
+# gunicorn -b 0.0.0.0:8000 --timeout 600 app:app
+
+#!/bin/bash
+
+export FLASK_APP=app.py
+export FLASK_ENV=production
+
+# Jalankan Flask server
+python3 -m flask run --host=0.0.0.0 --port=8000
